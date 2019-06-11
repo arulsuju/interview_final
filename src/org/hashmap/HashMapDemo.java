f719cf77a3dd7a4777ecd0fef221cf09ad5f4279
Arul Suju <arulsuju@yahoo.co.in>
2019-06-11 23:38:11 +0530

    CODE CHANGED

---
+++ src/org/hashmap/HashMapDemo.java
@@ -0,0 +1,20 @@
+package org.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+
+        Map<String,Integer> map=new HashMap<>();
+        map.put("a",1);
+        map.put("b",2);
+        map.put("a",3);
+        for(Map.Entry<String,Integer> m:map.entrySet()){
+            System.out.println(m.getKey()+"--->"+m.getValue());
+
+        }
+
+    }
+}
