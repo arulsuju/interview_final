a500a4164e846951c703842e54edd5373feb2e0b
Arul Suju <arulsuju@yahoo.co.in>
2019-06-11 23:47:25 +0530

    CODE CHANGED

--- src/org/hashmap/HashMapDemo.java
+++ src/org/hashmap/HashMapDemo.java
@@ -11,6 +11,8 @@ public class HashMapDemo {
         map.put("a",1);
         map.put("b",2);
         map.put("a",3);
+        map.put("a",1);
+        map.put(null,4);
         for(Map.Entry<String,Integer> m:map.entrySet()){
             System.out.println(m.getKey()+"--->"+m.getValue());
 
