e77800785ca70f5802bf585ffa205edb8bd27e6e
Arul Suju <arulsuju@yahoo.co.in>
2019-09-26 00:38:38 +0530

    CODE CHANGED

---
+++ src/org/thread/demo/SynchronizedThreadDemo.java
@@ -0,0 +1,23 @@
+package org.thread.demo;
+class Sender{
+    private String name;
+    private String msg;
+
+    public Sender(String name, String msg) {
+        this.name = name;
+        this.msg = msg;
+    }
+    public void display(){
+        System.out.println(name+msg);
+    }
+}
+class ThreadDemo1 extends Exception{
+
+
+}
+public class SynchronizedThreadDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
