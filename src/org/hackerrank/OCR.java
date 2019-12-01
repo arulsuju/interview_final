fbfca66af89c2e796b55ad9b9571590e14a2793c
Arul Suju <arulsuju@yahoo.co.in>
2019-12-02 03:34:11 +0530

    CODE CHANGE

--- src/org/hackerrank/OCR.java
+++ src/org/hackerrank/OCR.java
@@ -7,21 +7,33 @@ public class OCR {
         int K = 0;
         char c = '0';
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++)
+        {
             c = s.charAt(i);
-            if (Character.isLetter(c)) {
-                if (isNeedExpandQestionMark){
-                    for (int j = 0 ; j < K ; j++) {
+            System.out.println(c);
+            if (Character.isLetter(c))
+            {
+                if (isNeedExpandQestionMark)
+                {
+                    for (int j = 0 ; j < K ; j++)
+                    {
                         expanded.append('?');
                     }
                     isNeedExpandQestionMark = false;
                 }
                 expanded.append(c);
-            } else {
-                if(isNeedExpandQestionMark){
+            }
+            else
+            {
+                if(isNeedExpandQestionMark)
+                {
                     K = K * 10 + Character.getNumericValue(c);
-                } else {
+                    System.out.println(K);
+                }
+                else
+                {
                     K = Character.getNumericValue(c);
+                    //System.out.println("The number is"+K);
                 }
                 isNeedExpandQestionMark = true;
             }
@@ -32,7 +44,9 @@ public class OCR {
                 expanded.append('?');
             }
         }
+        System.out.println(expanded.toString());
         return expanded.toString();
+
     }
     public static boolean Solution(String s, String t) {
         final String expandedS = expand(s);
@@ -53,6 +67,8 @@ public class OCR {
 
     public static void main(String[] args) {
         OCR ocr=new OCR();
+        System.out.println(ocr.Solution("ba1","1Ad"));
+        System.out.println(ocr.Solution("A2Le","1ppLe"));
         System.out.println(ocr.Solution("ba1","1Ad"));
         //added comments
 
