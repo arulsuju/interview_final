9d8c681adac57cf0762f9d1f984397b433346ba1
Arul Suju <arulsuju@yahoo.co.in>
2020-05-16 19:21:43 +0530

    Join Demo posted

--- src/org/thread/demo/JoinDemo.java
+++ src/org/thread/demo/JoinDemo.java
@@ -6,6 +6,9 @@ class MyThread extends Thread{
         this.n=n;
     }
     public void run(){
+        Thread thread=Thread.currentThread();
+        thread.setName("ArulSuju");
+        System.out.println(thread.getName());
         for(int i=1;i<=n;i++){
             try {
                 Thread.sleep(1000);
@@ -21,9 +24,17 @@ public class JoinDemo {
     public static void main(String[] args) throws InterruptedException {
 
         MyThread myThread=new MyThread(10);
+        Thread thread=Thread.currentThread();
+        System.out.println(thread.getName());
+        long start=System.currentTimeMillis();
+        System.out.println(start);
         myThread.start();
         myThread.join();
+        long end=System.currentTimeMillis();
+        System.out.println(end);
+        long completionTime=(end-start)/1000;
         System.out.println("The sum is"+MyThread.sum);
+        System.out.println("Completion Time"+completionTime);
 
     }
 }
