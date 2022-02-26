76e447806217a37e8117d738322aecd4d7f870ab
arulsuju <arulsuju@yahoo.co.in>
2022-02-26 12:26:53 +0530

    added latest1

--- src/lamda/LamdaDemo.java
+++ src/lamda/LamdaDemo.java
@@ -6,11 +6,22 @@ interface FundTransfer{
 }
 public class LamdaDemo  {
     public static void main(String[] args) {
+        //With Lamda
         FundTransfer fl=(a,b)->{
             System.out.println(a+b);
         };
         fl.display(10,20);
 
+        //Without Lamda
+        FundTransfer fundTransfer;
+        fundTransfer = new FundTransfer() {
+            @Override
+            public void display(int a, int b) {
+                System.out.println(a+b);
+            }
+        };
+
+        fundTransfer.display(100,200);
     }
 
 
