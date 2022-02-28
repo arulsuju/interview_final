523432120a0f32568c4f1ee825bd24fde35d5dfc
arulsuju <arulsuju@yahoo.co.in>
2022-03-01 00:29:22 +0530

    UnModificableArrayList

--- src/UnModificableArrayList.java
+++ src/UnModificableArrayList.java
@@ -7,6 +7,17 @@ public class UnModificableArrayList {
     public static void main(String[] args) {
         List<Integer> list=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
         List<Integer> res= Collections.unmodifiableList(list);
-        res.add(8);
+       /* res.add(10);
+        Exception in thread "main" java.lang.UnsupportedOperationException
+        at java.util.Collections$UnmodifiableCollection.add(Collections.java:1057)
+        at UnModificableArrayList.main(UnModificableArrayList.java:10)*/
+
+        System.out.println(res);
+
+        final List<Integer> anotherList=list;
+        anotherList.add(10);
+        System.out.println(anotherList);
+        // [1, 2, 3, 4, 5, 6, 7, 10]
+
     }
 }
